bf1c4c69b9610e24f2a1c808e28df7140a807461
Bruno Moschetto <bruno.moschetto@gmail.com>
2024-10-14 16:40:42 +0200

    some changes to the challenge

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,37 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(tasks_params)
+    @task.save
+
+    redirect_to tasks_path
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(tasks_params)
+
+    redirect_to task_details_path(@task)
+  end
+
+  private
+
+  def tasks_params
+    params.require(:task).permit(:title, :description, :completed)
+  end
+end
